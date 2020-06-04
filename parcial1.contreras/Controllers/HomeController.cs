339628db0969345147f53ba60a79685e6c7f08d7
Favio Damian Contreras <FavioDamian.Contreras@Alumnos.uai.edu.ar>
2020-06-04 20:08:45 -0300

    vista index y css

---
+++ parcial1.contreras/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace parcial1.contreras.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
